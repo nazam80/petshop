35cd5308719c8553a8c3a3114a9203a61a65b7ff
Antonio Martínez López <nazam80@gmail.com>
2020-10-22 13:08:13 +0200

    Draw evey pet with every property in a row

--- static/main.js
+++ static/main.js
@@ -1,11 +1,34 @@
 
 
 const drawPets = () =>{ 
+
+    const createPetRowElement = (label, value)=>{
+        const row = document.createElement("div");
+        row.appendChild(document.createTextNode(label + value));
+        return row;
+    }
+
+    const createPetElement = (petData)=>{
+        const petDiv = document.createElement("div");
+        
+        petDiv.appendChild( createPetRowElement("Id: ", petData.id) );
+        petDiv.appendChild(createPetRowElement("Name: ", petData.name));
+        petDiv.appendChild(createPetRowElement("Breed: ", petData.breed));
+        petDiv.appendChild(createPetRowElement("Specie: ", petData.specie));
+
+        return petDiv;
+    }
+
     fetch('http://localhost:8080/pets')
     .then(response => response.json())
     .then(data => {      
         const petList = document.getElementById('ps-shop-pet-list');                
         petList.appendChild(data);
+
+        data.forEach((petData)=>{
+            const petDiv = createPetElement(petData);            
+            petList.appendChild(petDiv);
+        });        
 
     });
 
